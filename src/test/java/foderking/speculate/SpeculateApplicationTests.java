613f9409fdbf34d93424d233de6c67732b8600f0
foderking <foderking@gmail.com>
2024-11-20 14:50:04 +0100

    init project and add db config

---
+++ src/test/java/foderking/speculate/SpeculateApplicationTests.java
@@ -0,0 +1,13 @@
+package foderking.speculate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeculateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
