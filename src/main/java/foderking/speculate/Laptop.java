4dca0cf890a3a946f853d156cb59bafcd3c48cec
foderking <foderking@gmail.com>
2024-11-21 15:16:43 +0100

    fixed bug in parsing version 8 reviews

--- src/main/java/foderking/speculate/Laptop.java
+++ src/main/java/foderking/speculate/Laptop.java
@@ -3,8 +3,7 @@ package foderking.speculate;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.NodeFilter;
-
+import org.jsoup.select.Elements;
 import java.io.IOException;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -16,14 +15,22 @@ public record Laptop(
         String reviewDate,
         String picture,
         String model,
+        String reviewVersion,
         Map<String, String> info
 ) {
     public static String getReviewer(Document doc){
-            return doc
-                    .select("div.intro-author")
+        // reviews below v8 are parsed differently
+        Elements intro_author = doc.select(".intro-author");
+        if (intro_author.size() > 0) {
+            return intro_author
                     .first()
                     .text()
                     .split(",")[0];
+        // v8 and above
+        } else {
+            String tmp= doc.select("div.nbcintroelwide_abstract").first().childNode(3).outerHtml().strip();
+            return tmp;
+        }
     }
     public static String getReviewDate(Document doc){
         return doc
@@ -31,9 +38,19 @@ public record Laptop(
                 .attr("datetime");
     }
     public static String getPicture(Document doc){
-        return doc
-                .select(".csc-textpic-above > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > figure:nth-child(1) > img:nth-child(1)")
-                .attr("src");
+        // reviews below v8 are parsed differently
+        Elements orig = doc.select(".csc-textpic-above");
+        if (orig.size() > 0) {
+            return orig
+                    .first()
+                    .select("div:nth-child(1) > div:nth-child(1) > figure:nth-child(1) > img:nth-child(1)")
+                    .attr("src");
+        }
+        else{
+            return doc
+                    .select(".news-teaser-image picture img")
+                    .attr("src");
+        }
     }
     public static String getModel(Document doc){
         return doc
@@ -59,30 +76,28 @@ public record Laptop(
                 .collect(
                     Collectors.toMap(i -> (String) i[0], i -> (String)i[1])
                 );
-
     }
-
-
-
-    public static Optional<Laptop> create(String link){
-        Document doc;
+    public static Optional<Document> parse(String link){
         try {
-            doc = Jsoup
+            return Optional.of(Jsoup
                     .connect(link)
                     .userAgent("Mozilla")
-                    .get();
-            return Optional.of(
-                new Laptop(
-                    link,
-                    getReviewer(doc),
-                    getReviewDate(doc),
-                    getPicture(doc),
-                    getModel(doc),
-                    getInfo(doc)
-                )
-            );
+                    .get());
         } catch (IOException e) {
             return Optional.empty();
         }
+    }
+    public static Optional<Laptop> create(String link){
+        return parse(link).map(doc ->
+            new Laptop(
+                link,
+                Laptop.getReviewer(doc),
+                getReviewDate(doc),
+                getPicture(doc),
+                getModel(doc),
+                "",
+                getInfo(doc)
+            )
+        );
     }
 }
