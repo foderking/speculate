7a758f06fc21ee0df8060dadbb3a5a975872184e
foderking <foderking@gmail.com>
2024-11-25 14:26:51 +0100

    added proper parser to get all reviews from notebookcheck

--- src/main/java/foderking/speculate/Seeder.java
+++ src/main/java/foderking/speculate/Seeder.java
@@ -11,6 +11,7 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.time.Year;
 import java.util.List;
 import java.util.Optional;
 
@@ -44,23 +45,24 @@ public class Seeder implements CommandLineRunner {
             System.out.println("boobs");
         }
         else if (args.length == 1 && args[0].equals("update")) {
-            logger.info("running update seed");
-            Optional<Document> body = parseMinimal().map(Jsoup::parse);
-            logger.info("front page downloaded");
-            if (body.isPresent()) {
-                List<String> links = body.get()
-                        .select("table#search td > a")
-                        .eachAttr("href");
-                links.stream()
-                        .map(Laptop::create)
-                        .forEach(each -> {
-                            if (each.isPresent()) {
-                                System.out.println(each);
-                                repo.save(each.get());
-                                logger.info("data added to db");
-                            }
-                        });
-            }
+            populateDB();
+//            logger.info("running update seed");
+//            Optional<Document> body = parseMinimal().map(Jsoup::parse);
+//            logger.info("front page downloaded");
+//            if (body.isPresent()) {
+//                List<String> links = body.get()
+//                        .select("table#search td > a")
+//                        .eachAttr("href");
+//                links.stream()
+//                        .map(Laptop::create)
+//                        .forEach(each -> {
+//                            if (each.isPresent()) {
+//                                System.out.println(each);
+//                                repo.save(each.get());
+//                                logger.info("data added to db");
+//                            }
+//                        });
+//            }
         }
         else{
             logger.info("running no seed");
@@ -74,6 +76,57 @@ public class Seeder implements CommandLineRunner {
                     .uri(URI.create("https://www.notebookcheck.net/Laptop-Search.8223.0.html"))
                     .headers("Content-Type","application/x-www-form-urlencoded")
                     .POST(HttpRequest.BodyPublishers.ofString("manufacturer=&model=&lang=2&gpu=&gpu_manu=&gpu_architecture=&gpu_class=&cpu=&cpu_manu=&exclude_cpu_manu=&cpu_generation=&cpu_cores=&inch=&inch_from=&inch_to=&screen_ratio=&screen_resolution_x=&screen_resolution_y=&screen_glossy=&screen_panel_type=&screen_panel=&screen_refresh_rate=&class=&rating=92&reviewcount=&dr_workmanship=&dr_display=&dr_emissions=&dr_ergonomy=&dr_performance=&dr_mobility=&dr_temperature=&dr_audio=&dr_camera=&ratingversion=&age=&min_age=&year_from=&year_till=&weight=&size_width=&size_length=&size_depth=&price=&min_price=&min_list_price=&list_price=&ram=&battery_capacity=&battery_capacity_mah=&hdd_size=&hdd_type=&odd_type=&lan_type=&wlan_type=&brightness_center=&de2000_colorchecker=&percent_of_srgb=&percent_of_adobergb=&pwm=&loudness_min=&loudness_load=&battery_wlan=&os_type=&tag_type=16&nbcReviews=1&orderby=0&scatterplot_x=&scatterplot_y=&scatterplot_r="))
+                    .build(),
+                HttpResponse.BodyHandlers.ofString()
+            );
+            return Optional.of(tmp.body());
+        } catch(Exception e){
+            return Optional.empty();
+        }
+    }
+
+    public void populateDB(){
+        logger.info("scraping all laptop reviews");
+        int current_year = Year.now().getValue();
+
+        for (int year=2013; year <= current_year; year++) {
+            logger.info("Year: " + year);
+
+            Optional<List<String>> links = parseYear(year)
+                        .map(Jsoup::parse)
+                        .map(Laptop::createLinksFromSearch);
+            if (links.isPresent()) {
+                logger.info(links.get().size() + " links found");
+                links
+                    .get()
+                    .stream()
+                    .map(Laptop::create)
+                    .forEach(this::saveParsedLaptoptoDB);
+            }
+            else{
+                logger.info("failed to parse year: " + year);
+            }
+        }
+    }
+
+    public void saveParsedLaptoptoDB(Optional<Laptop> laptop){
+        if (laptop.isPresent()) {
+            repo.save(laptop.get());
+            logger.info("laptop saved to db");
+        }
+        else {
+            logger.info("laptop not parsed");
+        }
+    }
+
+    public Optional<String> parseYear(int year){
+        logger.info("Parsing reviews from " + year);
+        try {
+            var tmp = client.send(
+                HttpRequest.newBuilder()
+                    .uri(URI.create("https://www.notebookcheck.net/Laptop-Search.8223.0.html"))
+                    .headers("Content-Type","application/x-www-form-urlencoded")
+                    .POST(HttpRequest.BodyPublishers.ofString("manufacturer=&model=&lang=2&gpu=&gpu_manu=&gpu_architecture=&gpu_class=&cpu=&cpu_manu=&exclude_cpu_manu=&cpu_generation=&cpu_cores=&inch=&inch_from=&inch_to=&screen_ratio=&screen_resolution_x=&screen_resolution_y=&screen_glossy=&screen_panel_type=&screen_panel=&screen_refresh_rate=&class=&rating=&reviewcount=&dr_workmanship=&dr_display=&dr_emissions=&dr_ergonomy=&dr_performance=&dr_mobility=&dr_temperature=&dr_audio=&dr_camera=&ratingversion=&age=&min_age=&year_from=2022&year_till=2022&weight=&size_width=&size_length=&size_depth=&price=&min_price=&min_list_price=&list_price=&ram=&battery_capacity=&battery_capacity_mah=&hdd_size=&hdd_type=&odd_type=&lan_type=&wlan_type=&brightness_center=&de2000_colorchecker=&percent_of_srgb=&percent_of_adobergb=&pwm=&loudness_min=&loudness_load=&battery_wlan=&os_type=&tag_type=16&nbcReviews=1&archive=1&orderby=0&scatterplot_x=&scatterplot_y=&scatterplot_r="))
                     .build(),
                 HttpResponse.BodyHandlers.ofString()
             );
