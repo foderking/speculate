8e91e54e89296617c42a1d2eb6de006bab4d7f09
foderking <foderking@gmail.com>
2024-12-06 17:27:13 +0100

    enabled scheduling

--- src/main/java/foderking/speculate/Seeder.java
+++ src/main/java/foderking/speculate/Seeder.java
@@ -4,6 +4,7 @@ import org.jsoup.Jsoup;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -24,7 +25,6 @@ import java.util.function.Function;
 public class Seeder implements CommandLineRunner {
     LaptopRepository repo;
     HttpClient client;
-
     Logger logger = LoggerFactory.getLogger(Seeder.class);
     AtomicInteger success = new AtomicInteger(0);
     AtomicInteger error   = new AtomicInteger(0);
@@ -36,7 +36,6 @@ public class Seeder implements CommandLineRunner {
 
     @Override
     public void run(String... args){
-        logger.info("Started seeder");
         if (args.length == 1 && args[0].equals("update")) {
             logger.info("running manual seed");
         }
@@ -47,6 +46,11 @@ public class Seeder implements CommandLineRunner {
         else{
             logger.info("running no seed");
         }
+    }
+
+    @Scheduled(fixedRate = 5000)
+    public void update(){
+        System.out.println("update");
     }
 //    public void populateDB(){
 //        int max_concurrent = 5;
@@ -127,19 +131,19 @@ public class Seeder implements CommandLineRunner {
             success.set(0);
             logger.info("scraping reviews in " + year);
             Optional<List<String>> links =
-                    parseYear(year)
-                            .map(Jsoup::parse)
-                            .map(LaptopParser::createLinksFromSearch);
+                parseYear(year)
+                .map(Jsoup::parse)
+                .map(LaptopParser::createLinksFromSearch);
             if (links.isPresent()) {
                 logger.info(links.get().size() + " links found");
                 concurrentExecutor(
-                        max_concurrent, links.get(),
-                        link -> Laptop.create(link),
-                        laptop -> saveParsedLaptoptoDB(laptop),
-                        (e, link) -> {
-                            System.out.println(link);
-                            e.printStackTrace();
-                        }
+                    max_concurrent, links.get(),
+                    link -> Laptop.create(link),
+                    laptop -> saveParsedLaptoptoDB(laptop),
+                    (e, link) -> {
+                        System.out.println(link);
+                        e.printStackTrace();
+                    }
                 );
                 logger.info("success " + success.get() + ", error " + error.get());
             }
@@ -165,14 +169,15 @@ public class Seeder implements CommandLineRunner {
     }
 
     public Optional<String> parseYear(int year){
-        logger.info("Parsing reviews from " + year);
         try {
             var tmp = client.send(
                 HttpRequest.newBuilder()
                     .uri(URI.create("https://www.notebookcheck.net/Laptop-Search.8223.0.html"))
                     .headers("Content-Type","application/x-www-form-urlencoded")
                     .POST(HttpRequest.BodyPublishers.ofString(
-                            String.format("manufacturer=&model=&lang=2&gpu=&gpu_manu=&gpu_architecture=&gpu_class=&cpu=&cpu_manu=&exclude_cpu_manu=&cpu_generation=&cpu_cores=&inch=&inch_from=&inch_to=&screen_ratio=&screen_resolution_x=&screen_resolution_y=&screen_glossy=&screen_panel_type=&screen_panel=&screen_refresh_rate=&class=&rating=&reviewcount=&dr_workmanship=&dr_display=&dr_emissions=&dr_ergonomy=&dr_performance=&dr_mobility=&dr_temperature=&dr_audio=&dr_camera=&ratingversion=&age=&min_age=&year_from=%s&year_till=%s&weight=&size_width=&size_length=&size_depth=&price=&min_price=&min_list_price=&list_price=&ram=&battery_capacity=&battery_capacity_mah=&hdd_size=&hdd_type=&odd_type=&lan_type=&wlan_type=&brightness_center=&de2000_colorchecker=&percent_of_srgb=&percent_of_adobergb=&pwm=&loudness_min=&loudness_load=&battery_wlan=&os_type=&tag_type=16&nbcReviews=1&archive=1&orderby=0&scatterplot_x=&scatterplot_y=&scatterplot_r=", year, year)
+                        String.format(
+                            "manufacturer=&model=&lang=2&gpu=&gpu_manu=&gpu_architecture=&gpu_class=&cpu=&cpu_manu=&exclude_cpu_manu=&cpu_generation=&cpu_cores=&inch=&inch_from=&inch_to=&screen_ratio=&screen_resolution_x=&screen_resolution_y=&screen_glossy=&screen_panel_type=&screen_panel=&screen_refresh_rate=&class=&rating=&reviewcount=&dr_workmanship=&dr_display=&dr_emissions=&dr_ergonomy=&dr_performance=&dr_mobility=&dr_temperature=&dr_audio=&dr_camera=&ratingversion=&age=&min_age=&year_from=%s&year_till=%s&weight=&size_width=&size_length=&size_depth=&price=&min_price=&min_list_price=&list_price=&ram=&battery_capacity=&battery_capacity_mah=&hdd_size=&hdd_type=&odd_type=&lan_type=&wlan_type=&brightness_center=&de2000_colorchecker=&percent_of_srgb=&percent_of_adobergb=&pwm=&loudness_min=&loudness_load=&battery_wlan=&os_type=&tag_type=16&nbcReviews=1&archive=1&orderby=0&scatterplot_x=&scatterplot_y=&scatterplot_r=", year, year
+                        )
                     ))
                     .build(),
                 HttpResponse.BodyHandlers.ofString()
--- src/main/java/foderking/speculate/SpeculateApplication.java
+++ src/main/java/foderking/speculate/SpeculateApplication.java
@@ -3,10 +3,12 @@ package foderking.speculate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.net.http.HttpClient;
 
 @SpringBootApplication
+@EnableScheduling
 public class SpeculateApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpeculateApplication.class, args);
