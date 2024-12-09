9906f31bf539b445a891f552c74acf9ddb5ade8a
foderking <foderking@gmail.com>
2024-12-09 15:11:04 +0100

    improved readability of dropdown

--- src/main/java/foderking/speculate/SortParameters.java
+++ src/main/java/foderking/speculate/SortParameters.java
@@ -1,16 +1,26 @@
 package foderking.speculate;
 
 public enum SortParameters {
-    RATING,
-    BATTERY,
-    BRIGHTNESS,
-    COVERAGE_ADOBERGB,
-    COVERAGE_P3,
-    COVERAGE_SRGB,
-    MAX_TEMPERATURE_IDLE,
-    MAX_TEMPERATURE_LOAD,
-    RESPONSE_BW,
-    RESPONSE_GG,
-    WEIGHT,
-    THICKNESS
+    RATING("Rating"),
+    BATTERY("Battery Runtime"),
+    BRIGHTNESS("Brightness"),
+    COVERAGE_ADOBERGB("Adobe RGB Coverage"),
+    COVERAGE_P3("P3 Coverage"),
+    COVERAGE_SRGB("sRGB Coverage"),
+    MAX_TEMPERATURE_IDLE("Max Temp Idle"),
+    MAX_TEMPERATURE_LOAD("Max Temp Load"),
+    RESPONSE_BW("Response Time, Black-White"),
+    RESPONSE_GG("Response Time, Gray-Gray"),
+    WEIGHT("Weight"),
+    THICKNESS("Thickness");
+
+    private String value;
+
+    private SortParameters(String value){
+	this.value = value;
+    }
+    
+    public String getValue(){
+	return this.value;
+    }
 }
